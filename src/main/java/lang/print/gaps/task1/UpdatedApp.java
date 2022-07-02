7e9c7246e7d62266079a2a11a46ac7e2d6c2c9bc
NANO <File changed>
2022-07-02 23:36:07 +0600

    I have changed

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("I'm updated");
-        System.out.println("can be compiled. And can write to console");
+        System.out.print("I'm updated");
+        System.out.print("can be compiled. And can write to console");
     }
 }
