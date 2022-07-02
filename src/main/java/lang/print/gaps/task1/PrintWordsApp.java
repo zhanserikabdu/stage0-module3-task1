4c7e688dfcc45277139f0ff0c4d664fe0e5d882f
NANO <File changed>
2022-07-03 00:46:09 +0600

    I have changed

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,8 +2,7 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("java");
-        System.out.println("is");
-        System.out.println("smth");
+        System.out.println("java is smth");
+
     }
 }
