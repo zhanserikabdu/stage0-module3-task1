18a989ed4dae3be86a8a0a99dc8ea86f495ed488
NANO <File changed>
2022-07-03 01:07:05 +0600

    I have changed

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        System.out.println("This is my first java program");
+        System.out.println("This is my first Java program");
     }
 }
