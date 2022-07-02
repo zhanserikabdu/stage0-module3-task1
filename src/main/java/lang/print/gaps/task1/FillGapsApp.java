ed3a1dc4850bd6fbf40b446a7292c3869c64ee91
NANO <File changed>
2022-07-02 22:34:11 +0600

    I hope the tasks are correct

--- src/main/java/lang/print/gaps/task1/FillGapsApp.java
+++ src/main/java/lang/print/gaps/task1/FillGapsApp.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task1;
 public class FillGapsApp {
     public static void main(String[] args) {
 
-              .        ("Fixed");
+    System.out.println("Fixed");
     }
 }
